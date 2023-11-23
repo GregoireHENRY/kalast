5b59599af512bff1602cbfc47821a61521c74fb0
Grégoire Henry <gregoireh@pm.me>
2023-11-23 15:37:56 +0100

    Update docs cfg

--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -4,23 +4,25 @@
 ## To configure your scenarios.
 
 A scenario configuration is a `cfg/` folder containing the configuration files using [yaml][url-yaml] format.
-You can use an existing configuration from [the examples][examples] or write your own config.
+You can use an existing configuration from [the examples][examples] or write your own configuration.
+The `cfg/` folder must be located next to the executable.
 
-The configuration can be done in a single file or split in different files. 
+The configuration can be a single file or split in different files. 
 
 ### Using a single file `cfg.yaml`
 
-The name of the file must be `cfg.yaml`, inside the config folder (i.e., `cfg/cfg.yaml`).
-The config can be customised with the [optional fields of the main `Cfg` structure][Cfg#fields].
+The name of the file must be `cfg.yaml`, inside the configuration folder (i.e., `cfg/cfg.yaml`).
+The configuration can be customised with the [optional fields of the main `Cfg` structure][Cfg#fields].
 
-### Splitting the config into multiple files
+### Split the configuration into multiple files
 
 Each field of the main [`Cfg`] structure can written in a separate file. It leads to the following folder
 strcture:
 
-- `cfg/bodies/ *.yaml`: an additional folder called `bodies` in the config folder with as many config files inside
-as you want. Each file will be a new body, given that they are defined with unique name, using the [`CfgBody`
-configuration][CfgBody].
+- `cfg/bodies/ *.yaml`: an additional folder called `bodies` in the configuration folder with as many files inside as
+  you want for the configuration of the bodies.
+  Each file will be a new body, given that they are defined with unique name, using the
+  [`CfgBody` configuration][CfgBody].
 - `cfg/scene.yaml`: a file using the [`CfgScene` configuration][CfgScene].
 - `cfg/simulation.yaml`: a file using the [`CfgSimulation` configuration][CfgSimulation].
 - `cfg/window.yaml`: a file using the [`CfgWindow` configuration][CfgWindow]
