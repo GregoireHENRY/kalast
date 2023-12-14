331169951e0891805cff4a2658ce0ebbfaaeeef5
Grégoire Henry <gregoire.henry@oma.be>
2023-12-14 02:51:34 +0100

    wip

--- compile/sdl.sh
+++ compile/sdl.sh
@@ -3,6 +3,9 @@
 echo "matrix: $MATRIX_OS"
 echo "matrix: ${{ matrix.os }}"
 
+echo "runner: $RUNNER_OS"
+echo "runner: ${{ runner.os }}"
+
 if [ $MATRIX_OS == "ubuntu-latest" ]; then
     sudo apt install -y libsdl2-dev
 elif [ ${{ matrix.os }} == "macos-latest" ]; then
